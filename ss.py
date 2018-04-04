7114fc861131ecd21581508bc43c7de1cb414c8e
LeeJoongbe <35099709+LeeJoongbe@users.noreply.github.com>
2018-04-05 00:07:04 +0900

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,10 @@
+﻿#!C:\Users\skcon\AppData\Local\Programs\Python\Python36-32\python.exe
+
+a = 3+4+5
+b = a/3
+print("content-type:text/html; charset=UTF-8\n")
+print(b)
+
+
+d = input("말해주세요:")
+print(d)
